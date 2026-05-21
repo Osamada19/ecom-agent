d05b1e7bc7e6640c96a5c781cef442f40bd778ae
Osamada19 <oussamadardouri04gmail.com>
2026-05-21 17:01:25 +0100

    to gbt 4o mini

--- agent.py
+++ agent.py
@@ -248,11 +248,12 @@ Avoid robotic replies.
 
 ### "gpt-4o-mini"
 
+### "gemini-2.0-flash-001"
 
 
 ##openrouter
 llm = ChatOpenAI(
-   model="gemini-2.0-flash-001",
+   model="gpt-4o-mini",
    temperature=0,
    openai_api_key=os.getenv("OPENROUTER_API_KEY"), 
    openai_api_base="https://openrouter.ai/api/v1" 
