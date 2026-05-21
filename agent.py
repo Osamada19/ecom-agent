5511b76d265767fd7d2adf84ca94f8ac35de4af3
Osamada19 <oussamadardouri04gmail.com>
2026-05-21 15:50:28 +0100

    add footwear to kb

--- agent.py
+++ agent.py
@@ -100,13 +100,13 @@ Never assume or reuse names from previous context.
 
 
 
-#openrouter
-#llm = ChatOpenAI(
-   # model="gemini-2.0-flash-001",
-  #  temperature=0,
-  #  openai_api_key=os.getenv("OPENROUTER_API_KEY"), 
-  #  openai_api_base="https://openrouter.ai/api/v1" 
-#)
+##openrouter
+llm = ChatOpenAI(
+   model="gemini-2.0-flash-001",
+   temperature=0,
+   openai_api_key=os.getenv("OPENROUTER_API_KEY"), 
+   openai_api_base="https://openrouter.ai/api/v1" 
+)
 
 # #GBT LLM:
 # llm = ChatOpenAI(
